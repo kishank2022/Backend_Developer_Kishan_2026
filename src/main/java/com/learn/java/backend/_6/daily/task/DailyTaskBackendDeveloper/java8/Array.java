ac6e509bac345c5fc962ad8f36091a7ed5f67a80
kishank2022 <kishankushwaha076@gmail.com>
2026-03-16 12:59:47 +0530

    test

--- src/main/java/com/learn/java/backend/_6/daily/task/DailyTaskBackendDeveloper/java8/Array.java
+++ src/main/java/com/learn/java/backend/_6/daily/task/DailyTaskBackendDeveloper/java8/Array.java
@@ -3,6 +3,6 @@ package com.learn.java.backend._6.daily.task.DailyTaskBackendDeveloper.java8;
 public class Array {
 
 	public static void main(String[] args) {
-	System.out.println("Hello World");	
+	System.out.println("Hello World ok");	
 	}
 }
