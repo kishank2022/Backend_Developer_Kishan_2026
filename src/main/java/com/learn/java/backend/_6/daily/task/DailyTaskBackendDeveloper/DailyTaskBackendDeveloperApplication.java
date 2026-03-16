a65ab28cd733efa08bcdf164e522242680ce4392
kishank2022 <kishankushwaha076@gmail.com>
2026-03-16 12:52:10 +0530

    first commit

---
+++ src/main/java/com/learn/java/backend/_6/daily/task/DailyTaskBackendDeveloper/DailyTaskBackendDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.learn.java.backend._6.daily.task.DailyTaskBackendDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTaskBackendDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyTaskBackendDeveloperApplication.class, args);
+	}
+
+}
