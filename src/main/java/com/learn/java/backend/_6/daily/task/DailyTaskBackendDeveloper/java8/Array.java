3b3ae2adf0e8c32b426641c9f90d488320983481
kishank2022 <kishankushwaha076@gmail.com>
2026-03-16 14:13:06 +0530

    tst

---
+++ src/main/java/com/learn/java/backend/_6/daily/task/DailyTaskBackendDeveloper/java8/Array.java
@@ -0,0 +1,8 @@
+package com.learn.java.backend._6.daily.task.DailyTaskBackendDeveloper.java8;
+
+public class Array {
+
+	public static void main(String[] args) {
+	System.out.println("Hello World ok2");	
+	}
+}
---
+++ src/test/java/com/learn/java/backend/_6/daily/task/DailyTaskBackendDeveloper/DailyTaskBackendDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.java.backend._6.daily.task.DailyTaskBackendDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyTaskBackendDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
